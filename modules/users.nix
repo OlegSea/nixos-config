c7dd3a569f4844385ec058d1d1438ddfe4d28a8e
Alex Trifonov <ta.alexashow@ya.ru>
2025-10-29 18:26:27 +0300

    trust olegsea yes

--- modules/users.nix
+++ modules/users.nix
@@ -1,13 +1,20 @@
 { pkgs, ... }:
 
 {
+  nix.settings.trusted-users = [
+    "root"
+    "olegsea"
+  ];
   users.users.olegsea = {
-      isNormalUser = true;
-      description = "OlegSea";
-      extraGroups = [ "networkmanager" "wheel" ];
-      packages = with pkgs; [
+    isNormalUser = true;
+    description = "OlegSea";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    packages = with pkgs; [
       #  thunderbird
 
-      ];
-    };
+    ];
+  };
 }

be05e4ef08338c2926cd08c35fe65cd55ce39683
Alex Trifonov <ta.alexashow@ya.ru>
2025-11-08 14:09:06 +0300

    sync keepass

--- modules/network.nix
+++ modules/network.nix
@@ -32,6 +32,29 @@
     yt-dlp
   ];
 
+  services.syncthing =
+    let
+      username = "olegsea";
+    in
+    {
+      enable = true;
+      openDefaultPorts = true;
+      user = username;
+      configDir = "/home/${username}/.conifg/syncthing";
+      settings = {
+        folders = {
+          "Share" = {
+            path = "/home/olegsea/Share";
+            # devices = [
+            #   "oleg-laptop"
+            #   "oleg-phone"
+            # ];
+          };
+        };
+      };
+    };
+  networking.firewall.allowedTCPPorts = [ 8384 ];
+
   systemd.services.zapret = {
     description = "Zapret DPI Bypass";
     after = [ "network-online.target" ];

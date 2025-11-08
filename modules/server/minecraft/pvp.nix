314737079296ec50d0d067f70708c28560d336d2
Alex Trifonov <ta.alexashow@ya.ru>
2025-11-08 20:39:02 +0300

    another fix

--- modules/server/minecraft/pvp.nix
+++ modules/server/minecraft/pvp.nix
@@ -7,7 +7,7 @@
   services.minecraft-servers.servers.pvp = {
     enable = true;
     package = pkgs.paperServers.paper-1_21_4;
-    ops = {
+    operators = {
       olegsea = "f423d004-b056-4f79-87b3-a0fc33cb9acf";
     };
     serverProperties = {

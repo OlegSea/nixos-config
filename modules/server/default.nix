3b680ea128e281a11675fda8bab217e784adf5cd
Alex Trifonov <ta.alexashow@ya.ru>
2025-11-18 15:16:04 +0300

    allow 3000 again

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -9,11 +9,13 @@
   networking.firewall.allowedTCPPorts = [
     80
     443
+    3000
   ];
 
   networking.firewall.allowedUDPPorts = [
     80
     443
+    3000
   ];
 
   boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 0;

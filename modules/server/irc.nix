3bcccd6605ddc1393b90755318e540284060f9ee
Alex Trifonov <ta.alexashow@ya.ru>
2026-01-17 00:48:03 +0300

    ergochat

--- modules/server/irc.nix
+++ modules/server/irc.nix
@@ -1,23 +1,4 @@
 { pkgs, ... }:
 {
-  services.ircdHybrid = {
-    enable = true;
-
-    serverName = "irc.olegsea.ru";
-    description = "IRC Server by OlegSea";
-    adminEmail = "ta.alexashow@ya.ru";
-  };
-
-  networking.firewall = {
-    allowedTCPPorts = [
-      6667
-      6697
-      80
-    ];
-  };
-
-  systemd.services.ircd-hybrid.serviceConfig = {
-    Restart = "on-failure";
-    RestartSec = "5s";
-  };
+  services.ergochat.enable = true;
 }

6a4f5bcbd339f426d430b3228f0482b4aa5cddf5
Alex Trifonov <ta.alexashow@ya.ru>
2026-02-10 23:23:28 +0300

    no more lix

--- modules/common/core.nix
+++ modules/common/core.nix
@@ -15,18 +15,7 @@
     "elevator=mq-deadline"
   ];
 
-  nixpkgs.overlays = [
-    (final: prev: {
-      inherit (prev.lixPackageSets.stable)
-        nixpkgs-review
-        nix-eval-jobs
-        nix-fast-build
-        colmena
-        ;
-    })
-  ];
 
-  nix.package = pkgs.lixPackageSets.stable.lix;
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"

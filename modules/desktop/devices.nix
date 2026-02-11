212ae8d63d1092d8d5edb3494720846c427de571
Alex Trifonov <ta.alexashow@ya.ru>
2026-02-11 13:49:14 +0300

    try to fix otd

--- modules/desktop/devices.nix
+++ modules/desktop/devices.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }:
 {
   hardware.opentabletdriver.enable = true;
+  hardware.uinput.enable = true;
+  boot.kernelModules = [ "uinput" ];
 
   services.ratbagd.enable = true;
   environment.systemPackages = with pkgs; [

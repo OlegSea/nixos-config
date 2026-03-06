a22c3b852856d78a3a79e926656d7ef939c25dd7
Alex Trifonov <ta.alexashow@ya.ru>
2026-03-06 23:00:24 +0300

    new mouse so need software

--- modules/desktop/devices.nix
+++ modules/desktop/devices.nix
@@ -5,8 +5,10 @@
   boot.kernelModules = [ "uinput" ];
 
   services.ratbagd.enable = true;
+  hardware.logitech.wireless.enable = true;
   environment.systemPackages = with pkgs; [
     piper
     localsend
+    solaar
   ];
 }

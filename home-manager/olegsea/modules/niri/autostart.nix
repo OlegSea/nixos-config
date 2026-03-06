323a5710e66141669893658c19410073a229748b
Alex Trifonov <ta.alexashow@ya.ru>
2026-03-06 23:18:42 +0300

    Autostart fix

--- home-manager/olegsea/modules/niri/autostart.nix
+++ home-manager/olegsea/modules/niri/autostart.nix
@@ -4,9 +4,6 @@
   nixosConfigDir,
   ...
 }:
-let
-  noctalia = inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default;
-in
 {
   programs.niri.settings = {
     spawn-at-startup = [
@@ -17,19 +14,11 @@ in
           "${nixosConfigDir}/resources/wallpaper.png"
         ];
       }
-      # { argv = [ "${pkgs.waybar}/bin/waybar" ]; }
-      # { argv = [ "${pkgs.swaynotificationcenter}/bin/swaync" ]; }
-      # {
-      #   argv = [
-      #     "${noctalia}/bin/noctalia-shell"
-      #   ];
-      # }
       { argv = [ "${pkgs.vivaldi}/bin/vivaldi" ]; }
       { argv = [ "${pkgs.kitty}/bin/kitty" ]; }
-      { argv = [ "${pkgs.spotify}/bin/spotify" ]; }
       { argv = [ "${pkgs.telegram-desktop}/bin/Telegram" ]; }
-      { argv = [ "${pkgs.discord}/bin/discord" ]; }
-      { argv = [ "${pkgs.throne}/bin/throne" ]; }
+      { argv = [ "spotify" ]; }
+      { argv = [ "discord" ]; }
     ];
     hotkey-overlay = {
       skip-at-startup = true;

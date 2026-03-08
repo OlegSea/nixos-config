08b84d204b46424fd445ea3f282c923a5983b076
Alex Trifonov <ta.alexashow@ya.ru>
2026-03-09 00:54:55 +0300

    ai fix

--- modules/ai.nix
+++ modules/ai.nix
@@ -3,4 +3,10 @@
   environment.systemPackages = with pkgs; [
     lmstudio
   ];
+  networking.firewall.allowedTCPPorts = [
+    1234
+  ];
+  networking.firewall.allowedUDPPorts = [
+    1234
+  ];
 }

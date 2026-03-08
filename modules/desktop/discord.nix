3941296d4ec2f36c7b8d8de6aef2df53e65a7c61
Alex Trifonov <ta.alexashow@ya.ru>
2026-03-08 23:30:27 +0300

    discord

---
+++ modules/desktop/discord.nix
@@ -0,0 +1,80 @@
+{ pkgs, nixcord }:
+{
+  hm = {
+    imports = [ nixcord.homeModules.nixcord ];
+
+    programs.nixcord = {
+      enable = true;
+      discord.vencord.enable = true;
+      config = {
+        themeLinks = [
+          "https://raw.githubusercontent.com/shvedes/discord-gruvbox/refs/heads/main/gruvbox-dark.theme.css"
+        ];
+        frameless = true;
+        plugins = {
+          fakeNitro.enable = true;
+          betterGifPicker.enable = true;
+          betterGifAltText.enable = true;
+          betterSettings.enable = true;
+          blurNSFW.enable = true;
+          clearURLs.enable = true;
+          copyEmojiMarkdown.enable = true;
+          copyFileContents.enable = true;
+          copyStickerLinks.enable = true;
+          copyUserURLs.enable = true;
+          crashHandler.enable = true;
+          experiments.enable = true;
+          expressionCloner.enable = true;
+          favoriteGifSearch.enable = true;
+          fixYoutubeEmbeds.enable = true;
+          fixSpotifyEmbeds.enable = true;
+          fixImagesQuality.enable = true;
+          fullSearchContext.enable = true;
+          friendsSince.enable = true;
+          fullUserInChatBox.enable = true;
+          imageLink.enable = true;
+          imageZoom.enable = true;
+          memberCount.enable = true;
+          messageLogger.enable = true;
+          mutualGroupDMs.enable = true;
+          messageLinkEmbeds.enable = true;
+          messageClickActions.enable = true;
+          mentionAvatars.enable = true;
+          noF1.enable = true;
+          noOnboardingDelay.enable = true;
+          onePingPerDM = {
+            enable = true;
+            allowEveryone = true;
+            allowMentions = true;
+          };
+          openInApp.enable = true;
+          permissionFreeWill.enable = true;
+          permissionsViewer.enable = true;
+          petpet.enable = true;
+          quickReply.enable = true;
+          quickMention.enable = true;
+          reverseImageSearch.enable = true;
+          shikiCodeblocks.enable = true;
+          serverListIndicators.enable = true;
+          showHiddenChannels.enable = true;
+          showHiddenThings.enable = true;
+          showMeYourName.enable = true;
+          showTimeoutDuration.enable = true;
+          silentMessageToggle.enable = true;
+          sortFriendRequests.enable = true;
+          typingTweaks.enable = true;
+          unindent.enable = true;
+          userVoiceShow.enable = true;
+          validUser.enable = true;
+          validReply.enable = true;
+          viewRaw.enable = true;
+          viewIcons.enable = true;
+        };
+      };
+      extraConfig = {
+        # Some extra JSON config here
+        # ...
+      };
+    };
+  };
+}

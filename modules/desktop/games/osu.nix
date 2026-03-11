7ccaf4f510e0c07c6bff5e69df51801e594eb5cd
Alex Trifonov <ta.alexashow@ya.ru>
2026-03-11 20:42:44 +0300

    fix laggy audio in osu

--- modules/desktop/games/osu.nix
+++ modules/desktop/games/osu.nix
@@ -13,8 +13,8 @@ let
       postBuild = ''
         wrapProgram $out/bin/osu! \
           --set SDL_VIDEODRIVER wayland \
-          --set PIPEWIRE_LATENCY 128/44100 \
-          --set PIPEWIRE_ALSA "{ alsa.format=S32_LE alsa.rate=44100 alsa.channels=2 alsa.buffer-bytes=1024 alsa.period-bytes=256 }"
+          --set PIPEWIRE_LATENCY 128/48000 \
+          --set PIPEWIRE_ALSA "{ alsa.format=S32_LE alsa.rate=48000 alsa.channels=2 alsa.buffer-bytes=1024 alsa.period-bytes=256 }"
       '';
     }
   ) { };

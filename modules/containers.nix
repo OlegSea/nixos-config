1da0aebca5098d9a7aee44d10d9af28d48572354
Alex Trifonov <ta.alexashow@ya.ru>
2026-04-09 23:02:36 +0300

    docker

--- modules/containers.nix
+++ modules/containers.nix
@@ -1,19 +1,20 @@
 { pkgs, ... }:
 
 {
-  # virtualisation.docker = {
-  #   enable = true;
-  # };
-  virtualisation.podman = {
+  virtualisation.docker.rootless = {
     enable = true;
-    dockerCompat = true;
+    setSocketVariable = true;
   };
+  # virtualisation.podman = {
+  #   enable = true;
+  #   dockerCompat = true;
+  # };
 
-  users.groups.podman.members = [ "olegsea" ];
+  users.groups.docker.members = [ "olegsea" ];
 
   environment.systemPackages = with pkgs; [
     distrobox
-    podman-compose
+    # podman-compose
   ];
   environment.etc."distrobox/distrobox.conf".text = ''
     container_additional_volumes="/nix/store:/nix/store:ro /etc/profiles/per-user:/etc/profiles/per-user:ro /etc/static/profiles/per-user:/etc/static/profiles/per-user:ro"
